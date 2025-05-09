564ced5ebe0c3757e428566e469f45b6cd5eab07
ducdoan1806 <ducdoan1806@gmail.com>
2025-05-09 13:24:19 +0700

    update

--- decor/signals.py
+++ decor/signals.py
@@ -6,24 +6,11 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings
 from .models import ContactMessage, GoogleServiceAccount
 from django.utils import timezone
 
 logger = logging.getLogger(__name__)
 # Đường dẫn tới JSON key
-
-# ID của Google Sheet (phần giữa "/d/" và "/edit" trong URL)
-SPREADSHEET_ID = "1noay5wYR89Y3WuSSwcDRPhZxKG0g6EAL-1QVMh5q00c"
-
-# Thiết lập kết nối
-scope = ["https://www.googleapis.com/auth/spreadsheets"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    settings.CREDENTIALS_FILE, scope
-)
-gc = gspread.authorize(creds)
-
-sheet = gc.open_by_key(SPREADSHEET_ID).sheet1  # hoặc .worksheet('Tên sheet')
 
 
 def get_google_config():
